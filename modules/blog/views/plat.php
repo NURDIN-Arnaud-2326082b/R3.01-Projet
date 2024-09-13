32eb7136ab49e8961f204df02e3345e90feb0606
bapti <baptiste.armani@gmail.com>
2024-09-13 14:58:09 +0200

    Mis a joueur du plat, avec certains repas

--- modules/blog/views/plat.php
+++ modules/blog/views/plat.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../../../assets/styles/style.css">
-    <link rel="icon" href="../../../img/logo_tenrac.png">
+    <link rel="icon" href="../../../img/logo_tenrac.jpg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tenrac</title>
 </head>
@@ -15,25 +15,98 @@
             <button  onclick="openMenu()" class="header-menu-mobile">
                 <span class="material-icons">menu</span>
             </button>
-            <a class="header-logo" href="#">
+            <a class="header-logo" href="../../../index.php">
                 <img src="../../../img/logo_tenrac.png">
             </a>
             <ul class="header-menu">
-                <li><a href="structure.php">Structure</a></li>
+                <li><a href="../../../structure.php">Structure</a></li>
                 <li><a href="repas.php">Repas</a></li>
-                <li><a href="plat.php">Plat</a></li>
-                <li><a href="authentification.php">Authentification</a></li>
+                <li><a href="../../../connexion.php">Connexion</a></li>
             </ul>
         </div>
     </div>
 </header>
 <div id="overlay"></div>
 
-<main>
-    <p>
-        Ceci est la page des plats
-    </p>
-</main>
 
+
+<div class="slider">
+    <img  class="slider-background" src="../../../img/raclette.jpg" >
+    <div class="slider-content">
+        <h1>Tenrac</h1>
+        <p>Voici nos plats à base de raclette</p>
+    </div>
+</div>
+
+<div  id="about" class="section">
+    <div class="boxed">
+        <div class="flex toColumn mCenter">
+            <div  class="w40 wm100">
+                <h2>blablabla</h2>
+
+            </div>
+            <div class="w60 wm100">
+                <p>blablabla</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="skills" class="section dark">
+    <div class="boxed text-center">
+        <h2>Nos plats</h2>
+        <div class="flex toColumn gap20">
+            <div class="w25 wm100 skill">
+                <img src="../../../img/Tenders.png">
+                <h3>Tenders</h3>
+            </div>
+            <div class="w25 wm100 skill">
+                <img src="../../../img/Chips.png">
+                <h3>Chips</h3>
+            </div>
+            <div class="w25 wm100 skill">
+                <img src="../../../img/Hamburger.png">
+                <h3>Hamburgers</h3>
+            </div>
+            <div class="w25 wm100 skill">
+                <img src="../../../img/Tacos.png">
+                <h3>Tacos</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div  id="contact" class="section_dark">
+    <div class="boxed text-center">
+        <h2>blablabla</h2>
+        <div class="flex space-between">
+            <div class="w32  wm100 realisation">
+                <img src="../../../img/chess.jpg">
+                <h3>blablabla</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div  id="contact" class="section dark">
+    <div class="boxed">
+        <h2>Me contacter</h2>
+        <form id="contact-form">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="nom" required placeholder="Entrer votre nom">
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required placeholder="Entrer votre email">
+            </div>
+            <div class="form-group">
+                <label for="message">Message:</label>
+                <textarea id="message" name="message" rows="4" required placeholder="Entrer votre message"></textarea>
+            </div>
+            <button type="submit">Envoyer</button>
+        </form>
+    </div>
+</div>
 </body>
 </html>
