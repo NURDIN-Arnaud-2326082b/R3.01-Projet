c6c3e984e438f573d39e223fd274de8a7879db18
Vernagut <titouanvernagut@gmail.com>
2024-09-17 17:20:55 +0200

    commence php Repas

--- modules/blog/views/repas.php
+++ modules/blog/views/repas.php
@@ -3,6 +3,13 @@
 $css_files = "Repas.css";
 require '../controllers/header.php';
 header_page($page_title, $css_files);*/
+
+$cavalier_present = false;  // Vous pouvez ajuster cette condition selon vos besoins
+$dame_presente = false;     // Vous pouvez ajuster cette condition selon vos besoins
+
+// Obtenir la date actuelle
+$date_rencontre = date("d-m-Y H:i:s");
+$lieu_renconte="Salle de conférence, Hôtel XYZ";
 ?>
 <!DOCTYPE html>
 <html>
@@ -38,9 +45,19 @@ header_page($page_title, $css_files);*/
         présence d'un/d'une Cavalier/Dame:
     </h2>
     <p>
-        <img class="imgrdv" src="../../../img/Cavalier.png" height="300px" width="300px"  alt="Logo HubSpot">
-        blablabla
-        <img class="imgD" src="../../../img/Dame.png" height="250px" width="350px"  alt="Logo HubSpot">
+        <img class="imgrdv" src="../../../img/Cavalier.png" height="300px" width="300px" alt="Cavalier">
+        <?php if ($cavalier_present ): ?>
+            <span>Cavalier présent</span>
+        <?php else: ?>
+            <span>Aucun cavalier présent</span>
+        <?php endif; ?>
+        <img class="imgD" src="../../../img/Dame.png" height="250px" width="350px" alt="Dame">
+        <?php if ($dame_presente): ?>
+
+            <span>Dame présente</span>
+        <?php else: ?>
+            <span>Aucune dame présente</span>
+        <?php endif; ?>
     </p>
 </div>
 <section>
@@ -50,7 +67,9 @@ header_page($page_title, $css_files);*/
         </h2>
         <p>
             <img class="imgrdv" src="../../../img/rdv.png" height="200px" width="200px"  alt="Logo HubSpot">
-            blablabla
+            <?php if ($cavalier_present || $dame_presente): ?>
+                <span>lieu de présence : <?php echo $lieu_renconte; ?></span>
+            <?php endif; ?>
         </p>
     </div>
     <div class="divi">
@@ -60,7 +79,12 @@ header_page($page_title, $css_files);*/
         <div>
             <p>
                 <img class="imgrdv" src="../../../img/date.png" height="200px" width="200px"  alt="Logo HubSpot">
-                blablabla
+                <?php if ($cavalier_present || $dame_presente): ?>
+
+                    <span>Date de présence : <?php echo $date_rencontre; ?></span>
+                <?php endif; ?>
+
+
             </p>
         </div>
     </div>
