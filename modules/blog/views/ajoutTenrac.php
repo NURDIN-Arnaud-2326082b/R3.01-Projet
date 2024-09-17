7577e452e777a515584848c7e9c36d15dc52a683
Ronan <ronan.garrigues@etu.univ-amu.fr>
2024-09-17 16:47:23 +0200

    Update fichier ajoutTenrac

--- modules/blog/views/ajoutTenrac.php
+++ modules/blog/views/ajoutTenrac.php
@@ -1,4 +1,20 @@
-<form action="index.php?controller=tenrac&action=ajouter" method="POST">
+
+<?php
+$page_title = "ajoutTenrac";
+$css_files = "connexion.css";
+include __DIR__ . '/../controllers/header.php';
+header_page($page_title, $css_files);
+require_once '../models/db_connect.php'; // Connexion à la base de données
+require_once '../models/TenracModel.php'; // Modèle d'utilisateur
+require_once '../controllers/TenracController.php'; // Contrôleur d'utilisateur
+
+// Créez une instance du modèle et du contrôleur
+$userModel = new TenracModel($conn);
+$userController = new TenracController($userModel);
+
+?>
+
+<form action="index.php?controller=tenrac&action=ajouterTenrac" method="POST">
     <label for="nom">Nom:</label>
     <input type="text" name="nom" required><br>
     <label for="email">Email:</label>
@@ -11,3 +27,5 @@
     <input type="text" name="grade" required><br>
     <button type="submit">Ajouter Tenrac</button>
 </form>
+
+<?php $userController->ajouterTenrac();?>
