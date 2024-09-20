d229922195a2288d69745d8e62b3582221377417
Manon VERHILLE <145963847+VERHILLE-Manon-2326111mv@users.noreply.github.com>
2024-09-20 17:04:44 +0200

    Style de Structure.php terminé

--- modules/blog/views/structure.php
+++ modules/blog/views/structure.php
@@ -15,11 +15,32 @@ header_page("Structure", $css_files);
 
 <div id="ordre_tenrac">
     <h2>L'Ordre</h2>
-    <p class="sous_titre_club">Le royaume des Tenrac</p>
+    <p id="sous_titre">Le royaume des Tenrac</p>
+    <p>Description</p>
 </div>
 
-<div id ="Autres_clubs">
+<div id ="autres_clubs">
     <h2>Autres clubs</h2>
+    <div class="descri_club">
+        <h3>NomClub</h3>
+        <p>Adresse</p>
+    </div>
+
+    <div class="descri_club">
+        <h3>Club</h3>
+        <p>Adresse</p>
+    </div>
+
+    <div class="descri_club">
+        <h3>Club</h3>
+        <p>Adresse</p>
+    </div>
+
+    <div class="descri_club">
+        <h3>Club</h3>
+        <p>Adresse</p>
+    </div>
+
 </div>
 
 <?php
