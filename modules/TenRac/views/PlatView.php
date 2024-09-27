685b95de4b5eb495eb6f383c6d247d2018201554
Arnaud <arnaud.nurdin@etu.univ-amu.fr>
2024-09-27 16:00:04 +0200

    implémentation totale de l'affichage des plats et des ingréients qu'ils contiennent (sans css)

--- modules/TenRac/views/PlatView.php
+++ modules/TenRac/views/PlatView.php
@@ -2,11 +2,31 @@
 
 namespace TenRac\views;
 
+use TenRac\models\DbConnect;
+use TenRac\models\PlatModel;
+
 class PlatView extends AbstractView
 {
     protected function body(): void
     {
         include __DIR__ . '/plat.php';
+
+        $platmodel = new PlatModel(new DbConnect());
+        $plats = $platmodel->creerListe();
+        foreach ($plats as $plat) {
+            $plt = implode(", ", $plat);
+            echo '<div id="listeplat"><p>' . $plt . "<br>";
+            $index = $platmodel->cherhceIdPlat($plt);
+            $idx = $index[0]['Id_Plat'];
+            $ingredients = $platmodel->trouverIngredient((int)$idx);
+            $taille = count($ingredients);
+            foreach ($ingredients as $ingredient){
+                echo implode(",",$ingredient)."<br>";
+            }
+            echo "</p></div>";
+        }
+
+        include __DIR__ . '/platpart2.php';
     }
 
     function css(): string
