5576b77abe3f57edefe9003e47e819020c864833
Manon VERHILLE <145963847+VERHILLE-Manon-2326111mv@users.noreply.github.com>
2024-09-29 17:44:13 +0200

    Tentative gérer cases vides update.

--- modules/TenRac/views/structure.php
+++ modules/TenRac/views/structure.php
@@ -6,32 +6,5 @@
     </div>
 </div>
 
-<div id="ordre_tenrac">
-    <h2>L'Ordre</h2>
-    <p id="sous_titre">Le royaume des Tenrac</p>
-    <p>Description</p>
-</div>
-
 <div id ="autres_clubs">
-    <h2>Autres clubs</h2>
-    <div class="descri_club">
-        <h3>NomClub</h3>
-        <p>Adresse</p>
-    </div>
-
-    <div class="descri_club">
-        <h3>Club</h3>
-        <p>Adresse</p>
-    </div>
-
-    <div class="descri_club">
-        <h3>Club</h3>
-        <p>Adresse</p>
-    </div>
-
-    <div class="descri_club">
-        <h3>Club</h3>
-        <p>Adresse</p>
-    </div>
-
-</div>
+    <h2>Les Clubs</h2>
