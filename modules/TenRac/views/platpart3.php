f88a6283bae93257adae16c06a66b6d8e3b3bfd8
NURDIN-Arnaud-2326082b <arnaud.nurdin@etu.univ-amu.fr>
2024-09-30 17:11:44 +0200

    test avec nouveau js (impossible de faire sans c'est du côté client)

--- modules/TenRac/views/platpart3.php
+++ modules/TenRac/views/platpart3.php
@@ -1,4 +1,23 @@
-<button type="button" action="/ajouter">+</button>
+<script>
+    // Fonction JavaScript pour ajouter une nouvelle combobox
+    function ajouterCombobox() {
+        // Crée un élément div pour la nouvelle combobox
+        var newDiv = document.createElement('div');
+
+        // Effectue une requête AJAX pour appeler la méthode PHP
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'index.php?action=ajouter', true); // Appelle la méthode recupIngredient du contrôleur via la route
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // Injecte la réponse (le <select> généré par PHP) dans le div
+                newDiv.innerHTML = xhr.responseText;
+                document.getElementById('addcombo').appendChild(newDiv);
+            }
+        };
+        xhr.send();
+    }
+</script>
+<button type="button"  onclick="ajouterCombobox()">+</button>
 <button type="submit">Ajouter</button>
 </form>
 </div>
