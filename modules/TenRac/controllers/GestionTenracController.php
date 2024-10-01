c47bceb18b520a8c345e3ac1b83a713e2daf68b7
ptiga <ronan.garrigues@etu.univ-amu.fr>
2024-10-01 22:50:44 +0200

    Remise de phpDoc

--- modules/TenRac/controllers/GestionTenracController.php
+++ modules/TenRac/controllers/GestionTenracController.php
@@ -5,9 +5,24 @@ use TenRac\models\DbConnect;
 use TenRac\models\GestionTenracModel;
 use TenRac\views\GestionTenracView;
 
+/**
+ * Contrôleur pour la gestion des utilisateurs (Tenracs).
+ *
+ * Cette classe permet d'afficher la page de gestion des Tenracs,
+ * ainsi que d'ajouter, supprimer et modifier un utilisateur.
+ *
+ * @package TenRac\controllers
+ */
 class GestionTenracController
 {
 
+    /**
+     * Affiche la page de gestion des Tenracs.
+     *
+     * Cette méthode démarre une session utilisateur et affiche la vue de gestion des Tenracs.
+     *
+     * @return void
+     */
     public static function affichePage(): void{
         session_start();
         $view = new GestionTenracView();
@@ -15,6 +30,15 @@ class GestionTenracController
     }
 
 
+    /**
+     * Ajoute un nouvel utilisateur Tenrac dans la base de données.
+     *
+     * Cette méthode récupère les informations d'un Tenrac via une requête POST,
+     * les envoie au modèle pour les ajouter dans la base de données,
+     * envoie un email de bienvenue, puis redirige vers la page d'accueil.
+     *
+     * @return void
+     */
     public function ajouterTenrac(): void
     {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -51,6 +75,14 @@ class GestionTenracController
 
     }
 
+    /**
+     * Supprime un utilisateur Tenrac de la base de données.
+     *
+     * Cette méthode récupère le courriel d'un Tenrac via une requête POST
+     * et le supprime de la base de données si l'utilisateur est trouvé.
+     *
+     * @return void
+     */
     public function supprimerTenrac(): void
     {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -67,6 +99,15 @@ class GestionTenracController
         }
     }
 
+    /**
+     * Modifie les informations d'un utilisateur Tenrac.
+     *
+     * Cette méthode récupère les informations mises à jour d'un Tenrac via une requête POST,
+     * puis appelle le modèle pour effectuer la modification dans la base de données.
+     * Redirige ensuite vers la page d'accueil.
+     *
+     * @return void
+     */
     public function modifierTenrac(): void
     {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
