e4378d4579d3a03e0169cb24ee332ca33f062896
Manon VERHILLE <145963847+VERHILLE-Manon-2326111mv@users.noreply.github.com>
2024-10-02 19:02:28 +0200

    Retour du css pour la présentation des clubs lorsqu'on n'est pas connecté.

--- modules/TenRac/views/structure.php
+++ modules/TenRac/views/structure.php
@@ -1,5 +1,5 @@
 <div class="slider">
-    <img  class="slider-background" alt="Image de raclette" src="../../../img/raclette.jpg" >
+    <img  class="slider-background" alt="Image de raclette" src="../../../img/wallpaperStructure.jpg" >
     <div class="slider-content">
         <h1>La structure des Tenracs</h1>
         <p>Un vrai petit monde de passionnés</p>
--- modules/TenRac/views/structureTenrac.php
+++ modules/TenRac/views/structureTenrac.php
@@ -2,6 +2,7 @@
         <img  class="slider-background" alt="Image de raclette" src="../../../img/wallpaperStructure.jpg" >
         <div class="slider-content">
             <h1>La structure des Tenracs</h1>
+            <p>Un vrai petit monde de passionnés</p>
         </div>
     </div>
 
